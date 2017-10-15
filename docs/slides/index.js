9c7ce70de610df2bff67b71393018aa21a8ffaea
Ruben Gomez <rgomez.bertoli@gmail.com>
2017-10-15 19:36:07 +0200

    [FEAT] Acabadas slides

--- docs/slides/index.js
+++ docs/slides/index.js
@@ -7,7 +7,6 @@ const intro = [
     '<white-space><center>Profesor: 📚  Rubén Gómez Bertoli</center>',
     '<white-space><center>Correo: 💾  ruben.gomezbertoli@altran.com</center>',
     '<white-space><center>Twitter: 👨  @rgomezbertoli</center>'
-
 ];
 
 const indice = [
@@ -25,17 +24,20 @@ const nodejs = [
     `<center>${colors.blue('NodeJS')}</center>`,
     '<br>',
     '<white-space>',
-    `Es un entorno de desarrollo que corre sobre el motor ${colors.cyan.bold('V8')} desarrollado por Google en 2014`,
-    '<white-space>',
-    `Esta basado en un modelo de operaciones de E/S orientado a eventos`,
+    `Es un entorno de desarrollo que corre sobre el motor ${colors.cyan.bold('V8')} desarrollado`,
+    `por Google en 2014 y está basado en un modelo de operaciones de E/S orientado a eventos.`,
+    `En su última versión ya tiene incorporado el estandar de JS, ECMAScript2017, ${colors.cyan.bold('ES7')}`,
     '<white-space>',
     `El propio ecosistema incluye un gestor de paquetes, ${colors.cyan.bold('NPM')},`,
     'que nos permite controlar de forma sencilla las dependencias de nuestro proyecto',
     '<white-space>',
-    `Actualmente, en su última versión ya tiene incorporado el estandar de JS, ECMAScript2017, ${colors.cyan.bold('ES7')}`,
-    '<white-space>',
     'URL:',
-    `${colors.green('https://nodejs.org/es/')}`
+    `${colors.green('https://nodejs.org/es/')}`,
+    '<white-space>',
+    'Para nosotros lanzar nuestros programas Javascript solo tenemos que lanzar este comando:',
+    '<white-space>',
+    `<center>${colors.cyan.bold('$ node path/to/script.js')}</center>`,
+    '<white-space>',
 ];
 
 const npm = [
@@ -61,36 +63,84 @@ const npm = [
     '<white-space>',
 ];
 
+const express = [
+    `<center>${colors.blue('ExpressJS')}</center>`,
+    '<br>',
+    '<white-space>',
+    `${colors.cyan.bold('ExpressJS')} es una ${colors.cyan.bold('librería Javascript')} para NodeJS que nos permite crear aplicaciones`,
+    `de servidor de manera sencilla`,
+    '<white-space>',
+    'Express nos ofrece una serie de herramientas con las que poder gestionar el direccionamiento',
+    'y las respuestas a los diferentes servicios que desarrollemos.',
+    'Los objetos principales de la librería son:',
+    '<white-space>',
+    `   1. ${colors.cyan.bold('Application')}: Nos permitirá configurar nuestra aplicación`,
+    '<white-space>',    
+    `   2. ${colors.cyan.bold('Router')}: Se encargará del manejo de las llamadas a los diferentes endpoints`,
+    '<white-space>',
+    `   3. ${colors.cyan.bold('Request')} y ${colors.cyan.bold('Response')}: Son los objetos que representan las peticiones que realizamos`,
+    '   a cada uno de los endpoint. El objeto Response es el encargado de generar la respuesta.',
+    '<white-space>',
+    'URL:',
+    `${colors.green('http://expressjs.com/')}`,
+];
+
+const mongo = [
+    `<center>${colors.blue('🍂  MongoDB')}</center>`,
+    '<br>',
+    '<white-space>',
+    `${colors.cyan.bold('MongoDB')} es una base de datos ${colors.cyan.bold('NoSQL')}, lo que quiere decir que todos los datos`,
+    `no son almacenados de forma relacional sino que se guardan como un objeto, ${colors.cyan.bold('JSON')}.`,
+    '<white-space>',
+    'A cada uno de estos objetos se le asigna un ID con el cual podríamos hacer las',
+    'relaciones para poder obtener las relaciones entre los diferentes objetos',
+    'y colecciones de la base de datos.',
+    '<white-space>',
+    'URL:',
+    `${colors.green('https://www.mongodb.com/es')}`,
+    '<white-space>',
+    'Para hacer más facil el trato de los datos en MongoDB, se suele utilizar el ODM,',
+    `${colors.cyan.bold('Mongoose')}, el cuál nos ofrece una serie de herramientas con las que podemos`,
+    `crear los modelos que guardaremos en la ${colors.cyan.bold('BBDD')}. 💻`,
+    '<white-space>',
+    'URL:',
+    `${colors.green('http://mongoosejs.com/docs/guide.html')}`, 
+];
+
 const end = [
     `<center>${colors.blue('FIN')}</center>`,
     '<br>',
     '<white-space>',
-    '    __.,,------.._',
-    '    ,"   _      _   "`.',
-    '   /.__, ._  -=- _"`    Y',
-    '  (.____.-.`      ""`   j',
-    "   VvvvvvV`.Y,.    _.,-'       ,     ,     ,",
-    `      Y    ||,   '"\         ,/    ,/    ./`,
-    "      |   ,'  ,     `-..,'_,'/___,'/   ,'/   ,",
-    ` ..  ,;,,',-'"\\,'  ,  .     '     ' ""' '--,/    .. ..`,
-    ",'. `.`---'     `, /  , Y -=-    ,'   ,   ,. .`-..||_|| ..",
-    "ff\\\\`. `._        /f ,'j j , ,' ,   , f ,  \\=\\ Y   || ||`||_..",
-    'l` \` `.`."`-..,-\' j  /./ /, , / , / /l \\   \\=\\l   || `\' || ||...',
-    '`  `   `-._ `-.,-/ ,\' /`"/-/-/-/-"\'\'\'"`.`.  `\'.\--`\'--..`\'_`\' || ,',
-    "          \"`-_,',  ,'  f    ,   /      `._    ``._     ,  `-.`'//         ,",
-    '        ,-"\'\' _.,-\'    l_,-\'_,,\'          "`-._ . "`. /|     `.\'\ ,       |',
-    "      ,',.,-'\"          \=) ,`-.         ,    `-'._`.V |       \ // .. . /j",
-    '      |f\\\\               `._ )-."`.     /|         `.| |        `.`-||-\\\\/',
-    '      l` \`                 "`._   "`--\' j          j\' j          `-`---\'',
-    '       `  `                     "`,-  ,\'/       ,-\'"  /',
-    `                               ,'",__,-'       /,, ,-'`,
-    "                               Vvv'            VVv'"
-]
+    colors.green('      __.,,------.._'),
+    colors.green('    ,"   _      _   "`.'),
+    colors.green('   /.__, ._  -=- _"`    Y'),
+    colors.green('  (.____.-.`      ""`   j'),
+    colors.green("   VvvvvvV`.Y,.    _.,-'       ,     ,     ,"),
+    colors.green(`      Y    ||,   '"\         ,/    ,/    ./`),
+    colors.green("      |   ,'  ,     `-..,'_,'/___,'/   ,'/   ,"),
+    colors.green(` ..  ,;,,',-'"\\,'  ,  .     '     ' ""' '--,/    .. ..`),
+    colors.green(",'. `.`---'     `, /  , Y -=-    ,'   ,   ,. .`-..||_|| .."),
+    colors.green("ff\\\\`. `._        /f ,'j j , ,' ,   , f ,  \\=\\ Y   || ||`||_.."),
+    colors.green('l` \` `.`."`-..,-\' j  /./ /, , / , / /l \\   \\=\\l   || `\' || ||...'),
+    colors.green('`  `   `-._ `-.,-/ ,\' /`"/-/-/-/-"\'\'\'"`.`.  `\'.\--`\'--..`\'_`\' || ,'),
+    colors.green("          \"`-_,',  ,'  f    ,   /      `._    ``._     ,  `-.`'//         ,"),
+    colors.green('        ,-"\'\' _.,-\'    l_,-\'_,,\'          "`-._ . "`. /|     `.\'\ ,       |'),
+    colors.green("      ,',.,-'\"          \=) ,`-.         ,    `-'._`.V |       \ // .. . /j"),
+    colors.green('      |f\\\\               `._ )-."`.     /|         `.| |        `.`-||-\\\\/'),
+    colors.green('      l` \`                 "`._   "`--\' j          j\' j          `-`---\''),
+    colors.green('       `  `                     "`,-  ,\'/       ,-\'"  /'),
+    colors.green(`                               ,'",__,-'       /,, ,-'`),
+    colors.green("                               Vvv'            VVv'"),
+    '<white-space>',    
+    '<center>HASTA PRONTO  ✋</center>'
+];
 
 module.exports = {
     intro,
     indice,
     nodejs,
     npm,
+    express,
+    mongo,
     end
 };
