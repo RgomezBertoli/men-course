371e298c427adb00a75da25d16701bf7eae7aef4
Ruben Gomez Bertoli <ruben.gomezb@28APO4104.local>
2017-10-17 19:58:37 +0200

    [FEAT] Ejercicio doble ruta

--- express/api/login/index.js
+++ express/api/login/index.js
@@ -2,10 +2,15 @@ const { Router } = require('express');
 
 const LoginRouter = Router();
 
+var usuario = {};
+
 LoginRouter.post('/login', function(req, res){
-    res.status(200).send({
-        description: 'Ha realizado login correctamente'
-    });
+    usuario = req.body;
+    res.status(200).send(usuario);
+});
+
+LoginRouter.get('/user', function(req, res){
+    res.send(usuario);
 });
 
 module.exports = LoginRouter;
