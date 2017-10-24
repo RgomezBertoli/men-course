a26cedb244f73e251236cb252b41a53578b56422
Ruben Gomez Bertoli <ruben.gomezb@28apo0915.altran.es>
2017-10-24 19:35:07 +0200

    [FEAT] Login conectando con mongo

--- express/api/login/index.js
+++ express/api/login/index.js
@@ -1,17 +1,25 @@
 const { Router } = require('express');
-const { encode } = require('../../middlewares/token'); 
+const { encode } = require('../../middlewares/token');
+const UserModel = require('../../models/user');
 const LoginRouter = Router();
 
-var usuario = {};
 
 LoginRouter.post('/login', function (req, res, next) {
-    usuario = req.body;
+    const keys = req.body && Object.keys(req.body);
 
-    if (usuario.pass === 'asdf1234') {
-        const token = encode(usuario);
+    if (keys && keys.length) {
+        UserModel.findOne(req.body, function (err, result) {
 
-        res.setHeader('token', token);
-        res.status(200).send(usuario);
+            if (!err && result) {
+                const token = encode(result);
+
+                res.setHeader('token', token);
+                res.status(200).send({ user: result });
+            } else {
+                const error = { description: 'User Does not exist' };
+                next(error);
+            }
+        });
     } else {
         const error = { description: 'Wrong Login' };
 
